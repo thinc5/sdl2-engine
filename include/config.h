e761b75095ea07e6bb361969f44ce02661ae4e94
thinc5 <thinc360@gmail.com>
2019-02-21 00:12:03 +1000

    initial commit
    problems:
    WinMain??? wtf windows
    move load surface to another file and make a load texture function
    fix core game logic
    docker alternative for build environment
    event handler
    texture manager
    animations

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WINDOW_TITLE "test"
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,19 +1,95 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "../include/config.h"
+
+SDL_Surface* loadSurface( char* path );
 
 /**
  * Entry point for Engine.
  */
-int main(int argc, char* argv[])
+int WinMain(int argc, char* argv[])
 {
-    // Lmao look tony u dumb this is how you use these options
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Event event;
+
+    // Init sdl
     if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
     }
 
-    /* ... */
+    // Create the window
+    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_RESIZABLE);
+
+    // Create the renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    // Load haha
+    SDL_Surface* alexS;
+    alexS = loadSurface("res/alex.jpg");
+    SDL_Texture* alexT = SDL_CreateTextureFromSurface(renderer, alexS);
+    SDL_FreeSurface(alexS);
+
+
+    // Main game loop
+    bool game = true;
+    while (game) {
+        
+        while (SDL_PollEvent(&event)) {
+            // If person wants to exit i.e. alt+f4 or clicking x
+            if (event.type == SDL_QUIT) {
+                // Clean up memory here and break main game loop
+                printf("Caught SDL_QUIT, exiting now...\n");
+                SDL_DestroyTexture(alexT);
+                game = false;
+            }
+
+            // Draw
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, alexT, NULL, NULL);
+            SDL_RenderPresent(renderer);
+
+        }
+    }
 
     SDL_Quit();
-
     return 0;
+}
+
+SDL_Surface* loadSurface( char* path )
+{
+    printf("loading %s\n..", path);
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+    }
+    else
+    {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, loadedSurface->format, 0 );
+        if( optimizedSurface == NULL )
+        {
+            printf( "Unable to optimize image %s! SDL Error: %s\n", path, SDL_GetError() );
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return optimizedSurface;
 }
